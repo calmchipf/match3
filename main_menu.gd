eeab4f2a7528b8578a6a24012e1e0014af7d0a52
Dosymzhan <157879347+calmchipf@users.noreply.github.com>
2024-06-30 09:07:30 +0500

    fixed transitions and resolution

--- main_menu.gd
+++ main_menu.gd
@@ -1,8 +1,12 @@
 extends CanvasLayer
 
+@onready var wait_timer = $WaitTimer
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	if $AnimationPlayer:
+		$AnimationPlayer.play("RESET")
 	pass # Replace with function body.
 
 
@@ -12,5 +16,9 @@ func _process(delta):
 
 
 func _on_button_start_pressed():
+	wait_timer.start(0.5)
+	if $AnimationPlayer:
+		$AnimationPlayer.play("transition")
+	await wait_timer.timeout
 	get_tree().change_scene_to_file("res://scene/game_window.tscn")
 	pass # Replace with function body.
--- scripts/Piece.gd
+++ scripts/Piece.gd
@@ -16,7 +16,7 @@ func _process(delta):
 
 func move(target):
 	var tween: Tween = create_tween();
-	tween.tween_property(self,"position",target,0.3).set_trans(Tween.TRANS_ELASTIC).set_ease(Tween.EASE_OUT);
+	tween.tween_property(self,"position",target,0.2).set_trans(Tween.TRANS_ELASTIC).set_ease(Tween.EASE_IN_OUT);
 
 func dim():
 	var sprite = get_node("Sprite2D");
